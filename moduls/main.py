233f59469b5584dcfa6e634202926a84f2d24b5a
WATUNEED <watyneed@gmail.com>
2023-05-01 18:30:51 +0400

    refactoring

--- moduls/main.py
+++ moduls/main.py
@@ -1,19 +1,37 @@
-import asyncio
-import datetime
+from time import sleep
 from os import environ
+import json
+import logging
+import logging.config
+import logging.handlers
+
+import asyncio
 
 from binance.client import Client
-import pandas as pd
-import ta
-from pandas import DataFrame
 from binance.exceptions import BinanceAPIException
-from time import sleep
+import ta
+
+from pandas import DataFrame
+import pandas as pd
+
+import pyfiglet
+from rich import print
 
 
 API_KEY = environ.get('API_KEY')
 SECRET = environ.get('SECRET')
+LOGGER = logging.getLogger('bot')
 
 HISTORY = []
+
+
+def init_logging():
+    logging.config.dictConfig(get_log_config())
+
+
+def get_log_config() -> dict:
+    with open('../LocalLogConfig.json', 'r') as config:
+        return json.load(config)
 
 
 def klines(symbol: str, interval: str, start_str: str) -> DataFrame:
@@ -30,7 +48,7 @@ def klines(symbol: str, interval: str, start_str: str) -> DataFrame:
         symbol_data.index = pd.to_datetime(symbol_data.index, unit='ms')
         return symbol_data
     except BinanceAPIException as e:
-        print(e)
+        LOGGER.exception(e)
         sleep(60)
         return klines(symbol, interval, start_str)
 
@@ -52,7 +70,7 @@ async def print_1h_change():
     h_change = (float(candles.Close[1]) - float(candles.Open[1])) * 100 / float(candles.Open[1])
 
     if abs(h_change) >= 1:
-        print(f'{h_change:.02}%')
+        LOGGER.debug(f'{h_change:.02}%')
 
     await asyncio.sleep(5)
     await print_1h_change()
@@ -70,24 +88,29 @@ async def entry_point_search(symbol: str, quantity: int, signal=buy_signal):
 
                 buy_price = float(symbol_data.Close[-1])
 
-                print(f'{datetime.datetime.now()} Buy signal. New futures order. Price: {buy_price}')
+                LOGGER.info(f'Buy signal. New futures order. Price: {buy_price} Quantity: {quantity}')
 
                 await entry_point_search(symbol, quantity, sell_signal)
             else:
                 # order = client.futures_create_order(symbol=symbol, side='SELL', type='MARKET', quantity=quantity)
 
                 sell_price = float(symbol_data.Close[-1])
-                HISTORY.append((buy_price, sell_price, 100 - (100 * buy_price) / sell_price))
+                percentage_change = 100 - (100 * buy_price) / sell_price
+                HISTORY.append((buy_price, sell_price, percentage_change))
                 apr = sum(order[2] for order in HISTORY)
                 positive = sum(1 if order[1] - order[0] >= 0 else 0 for order in HISTORY)
 
-                print(f'{datetime.datetime.now()} Sell signal. New futures order. Price: {sell_price}')
-                print(f'Sum all orders: {apr}%\nCount orders: {len(HISTORY)}\nPositive orders: {positive}')
+                LOGGER.info(f'Sell signal. New futures order. Price: {sell_price} Quantity: {quantity}')
+                LOGGER.info(f'Sum all orders: {apr}%\nCount orders: {len(HISTORY)}\nPositive orders: {positive}')
 
                 await entry_point_search(symbol, quantity, buy_signal)
 
 
 if __name__ == '__main__':
+    print(pyfiglet.figlet_format('K A M T O R', font='larry3d', width=400))
+    init_logging()
+    LOGGER.info('Bot was initialized.')
     client = Client(api_key=API_KEY, api_secret=SECRET)
+    LOGGER.info('Client was initialized.')
     asyncio.run(print_1h_change())
     asyncio.run(entry_point_search('ETHBTC', 0))
