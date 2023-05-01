e4e5c1ec4b82677596aa077022962402205b3288
WATUNEED <watyneed@gmail.com>
2023-05-01 17:47:08 +0400

    add async 1h change printing

--- moduls/main.py
+++ moduls/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 from os import environ
 
@@ -15,13 +16,13 @@ SECRET = environ.get('SECRET')
 HISTORY = []
 
 
-def klines(symbol: str) -> DataFrame:
+def klines(symbol: str, interval: str, start_str: str) -> DataFrame:
     """Requests pair data from binance api"""
     try:
         symbol_data = pd.DataFrame(client.get_historical_klines(
             symbol=symbol,
-            interval='1m',
-            start_str='40m UTC'
+            interval=interval,
+            start_str=start_str
         ))
         symbol_data = symbol_data.iloc[:, :6]
         symbol_data.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
@@ -31,7 +32,7 @@ def klines(symbol: str) -> DataFrame:
     except BinanceAPIException as e:
         print(e)
         sleep(60)
-        return klines(symbol)
+        return klines(symbol, interval, start_str)
 
 
 def buy_signal(symbol_data: DataFrame) -> bool:
@@ -44,32 +45,49 @@ def sell_signal(symbol_data: DataFrame) -> bool:
     return ta.trend.macd_diff(symbol_data.Close).iloc[-1] < 0 < ta.trend.macd_diff(symbol_data.Close).iloc[-2]
 
 
-def entry_point_search(symbol: str, quantity: int, signal=buy_signal):
+async def print_1h_change():
+    """Outputs the percentage of change in the price of ETH if
+    the price has changed by more than 1% in the last hour."""
+    candles = klines('ETHUSDT', '1h', '2h UTC')
+    h_change = (float(candles.Close[1]) - float(candles.Open[1])) * 100 / float(candles.Open[1])
+
+    if abs(h_change) >= 1:
+        print(f'{h_change:.02}%')
+
+    await asyncio.sleep(5)
+    await print_1h_change()
+
+
+async def entry_point_search(symbol: str, quantity: int, signal=buy_signal):
     """Waits for a buy or sell signal, when it is received, recursively changes the signal search strategy."""
     global HISTORY
     buy_price = 0
     while True:
-        symbol_data = klines(symbol)
+        symbol_data = klines(symbol, '1m', 'start_str')
         if signal(symbol_data):
             if signal.__name__ == 'buy_signal':
                 # order = client.futures_create_order(symbol=symbol, side='BUY', type='MARKET', quantity=quantity)
-                print(f'{datetime.datetime.now()} Buy signal. New futures order. Price: {symbol_data.Close[-1]}')
-                buy_price = symbol_data.Close[-1]
-                entry_point_search(symbol, quantity, sell_signal)
+
+                buy_price = float(symbol_data.Close[-1])
+
+                print(f'{datetime.datetime.now()} Buy signal. New futures order. Price: {buy_price}')
+
+                await entry_point_search(symbol, quantity, sell_signal)
             else:
                 # order = client.futures_create_order(symbol=symbol, side='SELL', type='MARKET', quantity=quantity)
-                print(f'{datetime.datetime.now()} Sell signal. New futures order. Price: {symbol_data.Close[-1]}')
-                sell_price = symbol_data.Close[-1]
-                HISTORY = (buy_price, sell_price, 100 - (100 * buy_price) / sell_price)
 
+                sell_price = float(symbol_data.Close[-1])
+                HISTORY.append((buy_price, sell_price, 100 - (100 * buy_price) / sell_price))
                 apr = sum(order[2] for order in HISTORY)
                 positive = sum(1 if order[1] - order[0] >= 0 else 0 for order in HISTORY)
 
+                print(f'{datetime.datetime.now()} Sell signal. New futures order. Price: {sell_price}')
                 print(f'Sum all orders: {apr}%\nCount orders: {len(HISTORY)}\nPositive orders: {positive}')
 
-                entry_point_search(symbol, quantity, buy_signal)
+                await entry_point_search(symbol, quantity, buy_signal)
 
 
 if __name__ == '__main__':
     client = Client(api_key=API_KEY, api_secret=SECRET)
-    entry_point_search('ETHBTC', 0)
+    asyncio.run(print_1h_change())
+    asyncio.run(entry_point_search('ETHBTC', 0))
